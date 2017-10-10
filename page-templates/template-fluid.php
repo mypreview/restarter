fd0383952df9541dfa2ae116aa701fd08a31e49e
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-10-11 01:57:48 +0400

    Created fluid width page and post template file

---
+++ page-templates/template-fluid.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying all pages with fluid width.
+ *
+ * Template name:		Fluid Template
+ * Template Post Type:	post, page
+ * @see 				https://codex.wordpress.org/Theme_Development
+ * @author  			Mahdi Yazdani
+ * @package 			Restarter
+ * @since 				1.0.1
+ */
+
+get_header();
+
+if (have_posts()):
+	if (is_page()):
+		get_template_part('loop-templates/content', 'page');
+	else:
+		get_template_part('loop-templates/content', 'post');
+	endif;
+else:
+	get_template_part('loop-templates/content', 'none');
+endif;
+
+get_footer();
