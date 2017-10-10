16236914fb1186ea6a0f2ea1f05ccc63ccd96326
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-10-11 01:57:34 +0400

    Created not-found page markup

---
+++ loop-templates/content-none.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template part for displaying a message that posts cannot be found.
+ *
+ * @see 			http://codex.wordpress.org/Template_Hierarchy
+ * @author  		Mahdi Yazdani
+ * @package 		Restarter
+ * @since 		    1.0.1
+ */
+/**
+ * Functions hooked into "restarter_before_post" action
+ *
+ * @hooked restarter_post_header       	      - 10
+ * @hooked restarter_post_wrapper_start       - 30
+ * @since 1.0.0
+ */
+do_action('restarter_before_post');
+
+// Start the Loop
+while (have_posts()): the_post();
+
+	do_action('restarter_before_post_content');
+
+	get_template_part('template-parts/none/content');
+
+endwhile;
+
+/**
+ * Functions hooked into "restarter_after_post" action
+ *
+ * @hooked restarter_post_wrapper_end       	  - 20
+ * @since 1.0.0
+ */
+do_action('restarter_after_post');
