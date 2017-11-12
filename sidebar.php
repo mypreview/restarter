52c9845f107cbaa8f18cebd3818fe6751da4cce3
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-11-12 17:16:03 +0400

    Updated container class names

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
  * @see 		https://codex.wordpress.org/Function_Reference/dynamic_sidebar
  * @author  	Mahdi Yazdani
  * @package 	Restarter
- * @since 		1.0.0
+ * @since 		1.1.0
  */
 
 if (!is_active_sidebar('sidebar')):
@@ -13,7 +13,7 @@ if (!is_active_sidebar('sidebar')):
 endif;
 ?>
 <!-- Sidebar -->
-<div class="col-lg-3 col-md-4 col-sm-5" role="complementary" itemscope="itemscope" itemtype="http://schema.org/WPSideBar">
+<div class="<?php echo esc_attr(apply_filters('restarter_sidebar_wrapper_cls', 'col-lg-3 col-md-4 col-sm-5')); ?>" role="complementary" itemscope="itemscope" itemtype="https://schema.org/WPSideBar">
 	<div class="padding-top visible-xs"></div>
 	<aside class="sidebar space-bottom">
 		<?php dynamic_sidebar('sidebar'); ?>
