b0ddaeb1cbd52995c5e7340a5fdf7785b21326d6
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-11-12 17:19:02 +0400

    Updated HTML markup

--- template-parts/none/content.php
+++ template-parts/none/content.php
@@ -4,12 +4,12 @@
  *
  * @author  		Mahdi Yazdani
  * @package 		Restarter
- * @since 		    1.0.1
+ * @since 		    1.1.0
  */
 ?>
 <div class="content-none">
 	<?php if (is_home() && current_user_can('publish_posts')): ?>
-	<h2><?php printf(esc_html__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'restarter') , esc_url(admin_url('post-new.php'))); ?></h2>
+	<h2><?php printf(esc_html__('Ready to publish your first post? %1$sGet started here%2$s.', 'restarter') , '<a href="' . esc_url(admin_url('post-new.php')) . '">', '</a>'); ?></h2>
 	<?php elseif (is_search()): ?>
 	<h2 class="space-bottom-2x"><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'restarter'); ?></h2>
 	<?php 
